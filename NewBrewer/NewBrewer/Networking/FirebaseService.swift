f6c065bf86ca57574cdb8f7bbc1800e7dcfbb475
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-04-11 15:49:57 -0600

    adds loadBeers function to FirebaseService.

--- NewBrewer/NewBrewer/Networking/FirebaseService.swift
+++ NewBrewer/NewBrewer/Networking/FirebaseService.swift
@@ -9,12 +9,19 @@ import Foundation
 import FirebaseFirestore
 import FirebaseStorage
 
-protocol FirebaseSyncable {
-    func saveBeer(beer: BeerToSave, with uuid: String, completion: @escaping (Result<Bool, NetworkError>) -> Void)
-    func delete(beer: BeerToSave)
+enum FirebaseError: Error {
+    case firebaseError(Error)
+    case failedToUnrwapData
+    case noDataFound
 }
 
-struct FirebaseService: FirebaseSyncable {
+protocol FirebaseServicable {
+    func saveBeer(beer: BeerToSave, with uuid: String, completion: @escaping (Result<Bool, FirebaseError>) -> Void)
+    func delete(beer: BeerToSave)
+    func loadBeers(completion: @escaping (Result<[BeerToSave], FirebaseError>) -> Void)
+}
+
+struct FirebaseService: FirebaseServicable {
     
     // MARK: - Properties
     let ref = Firestore.firestore()
@@ -22,12 +29,12 @@ struct FirebaseService: FirebaseSyncable {
     // Storage is only used for images
     
     // MARK: - Functions
-    func saveBeer(beer: BeerToSave, with uuid: String, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+    func saveBeer(beer: BeerToSave, with uuid: String, completion: @escaping (Result<Bool, FirebaseError>) -> Void) {
         
         ref.collection("beers").document(beer.uuid).setData(beer.dictionaryRepresentation) { error in
             if let error = error {
                 print(error.localizedDescription)
-                completion(.failure(NetworkError.thrownError(error)))
+                completion(.failure(.firebaseError(error)))
             }
             completion(.success(true))
         }
@@ -35,5 +42,21 @@ struct FirebaseService: FirebaseSyncable {
     
     func delete(beer: BeerToSave) {
         ref.collection("beers").document(beer.uuid).delete()
+    }
+    
+    func loadBeers(completion: @escaping (Result<[BeerToSave], FirebaseError>) -> Void) {
+        ref.collection("beers").getDocuments { snapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.failure(.firebaseError(error)))
+                return
+            }
+            
+            guard let docSnapshotArray = snapshot?.documents else { completion(.failure(.noDataFound)) ; return }
+            
+            let dictionaryArray = docSnapshotArray.compactMap { $0.data() }
+            let beer = dictionaryArray.compactMap { BeerToSave(fromDictionary: $0)}
+            completion(.success(beer))
+        }
     }
 }
