0a9ee2f98dd8a4af19e965a143e9e70ad8ae4fa0
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-04-20 16:31:19 -0600

    adds apple sign in.

--- NewBrewer/NewBrewer/Views/SignIn/SignInViewController.swift
+++ NewBrewer/NewBrewer/Views/SignIn/SignInViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
     
@@ -16,6 +18,7 @@ class SignInViewController: UIViewController {
     
     // MARK: - Properties
     var viewModel: SignInAccountViewModel!
+    var nonce: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,10 +43,56 @@ class SignInViewController: UIViewController {
         request.requestedScopes = [
             .email
         ]
+        
+        self.nonce = randomNonceString()
+        request.nonce = sha256(nonce!)
+        
         let authController = ASAuthorizationController(authorizationRequests: [request])
         authController.presentationContextProvider = self
         authController.delegate = self
         authController.performRequests()
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
     }
     
     // MARK: - Actions
@@ -63,10 +112,25 @@ extension SignInViewController: ASAuthorizationControllerPresentationContextProv
 
 extension SignInViewController: ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        <#code#>
+        
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        <#code#>
+        if let appleIdCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            // do something with this credential
+            guard let nonce = self.nonce else {
+              fatalError()
+            }
+            guard let appleIdToken = appleIdCredential.identityToken else {
+                print("Failed to get token.")
+                return
+            }
+            guard let idTokenString = String(data: appleIdToken, encoding: .utf8) else { print("No Token") ; return }
+            let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            Auth.auth().signIn(with: firebaseCredential) { [weak self] authResult, error in
+                // do something after firebase sign in has completed
+                
+            }
+        }
     }
 }
